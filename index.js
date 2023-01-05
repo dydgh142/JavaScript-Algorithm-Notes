81d40c7bfea0d6a4a80856e3f5b3f88ddbfdac1b
dydgh <dydgh179@gmail.com>
2023-01-05 15:07:25 +0900

    1264 수정

--- index.js
+++ index.js
@@ -3,19 +3,16 @@ const fs = require('fs');
 let a = fs.readFileSync('input.txt').toString().split('\n');
 
 const checker = ['a', 'e', 'i', 'o', 'u'];
-let count = [];
-for (let i = 0; i < a.length - 1; i++) {
-  count.push(0);
-}
 
 for (let i = 0; i < a.length - 1; i++) {
+  let count = 0;
   if (a[i] == '#') break;
   for (let char of a[i].toLowerCase()) {
     if (checker.includes(char)) {
-      count[i] += 1;
+      count += 1;
     }
   }
-  console.log(count[i]);
+  console.log(count);
 }
 // for (let j = 0; j < a.length - 1; j++) {
 //   console.log(count[j]);
