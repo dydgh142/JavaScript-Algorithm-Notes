cce1f20b06489656f36cea792dad4b65bbf0c9ad
dydgh <dydgh179@gmail.com>
2023-01-05 14:55:26 +0900

    1264 모음 갯수세기

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const checker = ['a', 'e', 'i', 'o', 'u'];
 let count = [];
 for (let i = 0; i < a.length - 1; i++) {
   count.push(0);
+}
+
+for (let i = 0; i < a.length - 1; i++) {
+  if (a[i] == '#') break;
   for (let char of a[i].toLowerCase()) {
     if (checker.includes(char)) {
       count[i] += 1;
     }
   }
+  console.log(count[i]);
 }
-for (let j = 0; j < a.length - 1; j++) {
-  console.log(count[j]);
-}
+// for (let j = 0; j < a.length - 1; j++) {
+//   console.log(count[j]);
+// }
